4d14a03db4baef0414e8a88eb74e70f2d0dea180
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-04-26 20:28:12 -0700

    Added query stub

---
+++ tag_h/tag_h/Persistence/Query/Query.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tag_h.Persistence.Query
+{
+
+    interface IQuery
+    {
+        void Execute();
+    }
+
+
+    class SaveImageQuery : IQuery
+    {
+        public void SetImage(HImage image)
+        {
+
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class FetchAllImages : IQuery
+    {
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class FetchImage : IQuery
+    {
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
