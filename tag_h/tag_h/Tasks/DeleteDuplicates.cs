02a7376a2bef6eb750e61dbddac06bab185772b2
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-04-26 23:26:54 -0700

    Added delete duplicate stub

---
+++ tag_h/tag_h/Tasks/DeleteDuplicates.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tag_h.Persistence;
+
+namespace tag_h.Tasks
+{
+    class DeleteDuplicates : ITask
+    {
+        public string TaskName => "Deleting Duplicate";
+
+        public void Execute(ImageDatabase database)
+        {
+            var dbImages = database.FetchAllImages();
+
+
+        }
+    }
+}
