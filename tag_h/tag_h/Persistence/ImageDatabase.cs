a1b2b418179abb2bc39d9c8997c0dc3e6df61ff5
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-04-28 00:35:25 -0700

    Refactored database

--- tag_h/tag_h/Persistence/ImageDatabase.cs
+++ tag_h/tag_h/Persistence/ImageDatabase.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tag_h.Persistence.Query;
 
 namespace tag_h.Persistence
 {
@@ -37,33 +38,12 @@ namespace tag_h.Persistence
 
         public void AddNewImage(string fileName)
         {
-            using (var command = _connection.CreateCommand())
-            {
-                command.CommandText
-                    = @"INSERT INTO Images (fileName, tags, viewed, deleted) 
-                        VALUES (@fileName, NULL, 0, 0);";
-                command.Parameters.AddWithValue("@fileName", fileName);
-                command.ExecuteNonQuery();
-            }
+            ExecuteQuery(new AddNewImageQuery(fileName));
         }
 
         public void SaveImage(HImage image)
         {
-            using (var command = _connection.CreateCommand())
-            {
-                command.CommandText
-                    = @"UPDATE Images
-                        SET fileName = @fileName,
-                            tags = @tags,
-                            viewed = 1
-                        WHERE id = @id;";
-                command.Parameters.AddWithValue("@id", image.UUID);
-
-                command.Parameters.AddWithValue("@fileName", image.Location);
-                command.Parameters.AddWithValue("@tags", string.Join(", ", image.Tags));
-
-                command.ExecuteNonQuery();
-            }
+            ExecuteQuery(new SaveImageQuery(image));
         }
 
         public List<HImage> GetImageFromTag()
@@ -77,87 +57,34 @@ namespace tag_h.Persistence
 
         public List<HImage> FetchAllImages()
         {
-            List<HImage> images = new List<HImage>();
-
-            using (var command = _connection.CreateCommand())
-            {
-                command.CommandText
-                    = @"SELECT * 
-                        FROM Images 
-                        WHERE deleted = 0;";
-
-                var dataReader = command.ExecuteReader();
-                while (dataReader.Read())
-                {
-                    images.Add(
-                            new HImage(dataReader.GetInt32(0), dataReader.GetString(1))
-                        );
-                }
-            }
-
-            return images;
+            return ExecuteQuery(new FetchAllImagesQuery()).Result;
         }
 
         public void DeleteImage(HImage image)
         {
-            using (var command = _connection.CreateCommand())
-            {
-                command.CommandText
-                    = @"UPDATE Images
-                        SET deleted = 1
-                        WHERE id = @id;";
-
-                command.Parameters.AddWithValue("@id", image.UUID);
-                command.ExecuteNonQuery();
-            }
+            ExecuteQuery(new DeleteImageQuery(image));
 
         }
 
-        public int ClearDeletedImages()
+        public void ClearDeletedImages()
         {
-            var images = GetDeletedImages();
-
-            using (var command = _connection.CreateCommand())
-            {
-                command.CommandText
-                    = @"DELETE FROM Images
-                        WHERE deleted = 1;";
-
-                command.ExecuteNonQuery();
-            }
-
-            foreach (var image in images)
-            {
-                if (File.Exists(image.Location))
-                {
-                    File.Delete(image.Location);
-                }
-            }
-
-            return images.Count();
+            ExecuteQuery(new ClearDeletedImagesQuery(this));
         }
 
         private List<HImage> GetDeletedImages()
         {
-            List<HImage> images = new List<HImage>();
+            return ExecuteQuery(new FetchDeletedImagesQuery()).Result;
+        }
 
+
+        public T ExecuteQuery<T>(T query) where T : IQuery
+        {
             using (var command = _connection.CreateCommand())
             {
-                command.CommandText
-                    = @"SELECT * 
-                        FROM Images 
-                        WHERE deleted = 1;";
-
-                var dataReader = command.ExecuteReader();
-                while (dataReader.Read())
-                {
-                    images.Add(
-                            new HImage(dataReader.GetInt32(0), dataReader.GetString(1))
-                        );
-                }
+                query.Execute(command);
             }
 
-            return images;
+            return query;
         }
     }
 }
