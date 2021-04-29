ad7333c46432f02a9c8a70d258c32b16c24cfa12
chally <chally-portable@DESKTOP-BUOJ68C>
2021-04-29 06:24:24 -0700

    Added HImageList

---
+++ tag_h/tag_h/Model/HImageList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tag_h.Model
+{
+    class HImageList
+    {
+        private List<HImage> _images;
+
+        private int pointer = 0;
+
+        public HImageList(List<HImage> images)
+        {
+            _images = images;
+        }
+
+        public HImage Get()
+        {
+            return _images[pointer];
+        }
+
+        public void MoveBack()
+        {
+            pointer = Math.Max(0, pointer - 1);
+        }
+
+        public void MoveForward()
+        {
+            pointer = Math.Min(_images.Count - 1, pointer + 1);
+            pointer = Math.Max(0, pointer);
+        }
+
+        public bool AtStart()
+        {
+            return pointer == 0;
+        }
+
+        public bool AtEnd()
+        {
+            return pointer == _images.Count - 1;
+        }
+    }
+}
