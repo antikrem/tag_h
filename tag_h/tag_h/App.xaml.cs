0f647f214498290741354a508b26e1d1c48c3b9d
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-05-08 11:11:13 -0700

    Removed extra comment

--- tag_h/tag_h/App.xaml.cs
+++ tag_h/tag_h/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace tag_h
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         private void Application_Startup(object sender, StartupEventArgs e)
