63e3ba0f5d355d708ee25d9a8c4faa7f39ca7876
chally <chally-portable@DESKTOP-BUOJ68C>
2021-05-12 01:24:31 -0700

    Added foreach extension for enumerables

---
+++ tag_h/tag_h/Helper/Extensions/CollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tag_h.Helper.Extensions
+{
+    static class CollectionExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> sequence, Action<T> action)
+        {
+            foreach (T t in sequence)
+            {
+                action(t);
+            }
+        }
+    }
+}
