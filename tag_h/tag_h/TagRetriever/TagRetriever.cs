afd58dce9c16178f97eccedaa79be77cba6d1a1b
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-06-10 00:06:40 -0700

    Added tag stub

---
+++ tag_h/tag_h/TagRetriever/TagRetriever.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tag_h.TagRetriever
+{
+    public interface ITagRetriever
+    {
+
+    }
+
+    public class TagRetriever : ITagRetriever
+    {
+
+
+
+    }
+}
---
+++ tag_h/tag_h/TagRetriever/TagSource/ITagSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tag_h.Model;
+
+namespace tag_h.TagRetriever.TagSource
+{
+    
+    public interface ITagSource
+    {
+
+        async TagSet RetrieveTags();
+    }
+}
