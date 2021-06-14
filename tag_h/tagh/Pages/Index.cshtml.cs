678e1bf814d27db872389b3e0fad1a47d880bcfe
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-06-13 17:16:23 -0700

    Added update for IndexModel

--- tag_h/tagh/Pages/Index.cshtml.cs
+++ tag_h/tagh/Pages/Index.cshtml.cs
@@ -1,20 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using tagh.Client.Model;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using tagh.Core.Model;
+using tagh.Core.Persistence;
+using Microsoft.AspNetCore.Mvc;
 
 namespace tagh.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IHImageRepository _imageRepository;
+        private readonly IHImageClientDataBuilder _imageClientDataBuilder;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IHImageRepository imageRepository, IHImageClientDataBuilder imageClientDataBuilder)
         {
             _logger = logger;
+            _imageRepository = imageRepository;
+            _imageClientDataBuilder = imageClientDataBuilder;
         }
 
         public void OnGet()
