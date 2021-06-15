d3309b61f3c0e50008e9d5da2a108fbc2039fae8
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-06-14 23:34:09 -0700

    Added controller for images

---
+++ tag_h/tag_h/Controllers/ImageBrowseController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using tag_h.Core.Model;
+using tag_h.Core.Persistence;
+
+namespace tag_h.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageBrowseController : ControllerBase
+    {
+
+        private readonly ILogger<TestController> _logger;
+        private readonly IHImageRepository _imageRepository;
+
+        public ImageBrowseController(ILogger<TestController> logger, IHImageRepository imageRepository)
+        {
+            _logger = logger;
+            _imageRepository = imageRepository;
+        }
+
+        [HttpGet]
+        public HImageList Get()
+        {
+            return _imageRepository.FetchImages(TagQuery.All);
+
+        }
+    }
+}
