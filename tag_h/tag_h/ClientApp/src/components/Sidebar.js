fd8a3b17dca68afb189d759b822ea86dc0d78db6
chally <chally-portable@DESKTOP-BUOJ68C>
2021-06-25 08:12:18 -0700

    Temp

--- tag_h/tag_h/ClientApp/src/components/Sidebar.js
+++ tag_h/tag_h/ClientApp/src/components/Sidebar.js
@@ -3,8 +3,22 @@ import { SidebarItem } from './SidebarItem';
 import './Sidebar.css';
 
 export class Sidebar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { open : true };
+        this.toggleOpen = this.toggleOpen.bind(this);
+    }
+
+    toggleOpen() {
+        this.setState({
+            open: !this.state.open
+        });
+    }
+
     render() {
         return (<div className="sidebar">
+            <button className="btn btn-primary" onClick={this.toggleOpen}>Close</button>
             <SidebarItem text="Home" link="/" />
             <SidebarItem text="Counter" link="/counter" />
             <SidebarItem text="Image View" link="/images-view" />
