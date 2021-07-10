abcc79e156119188de52cfdc098373afb6a6ef38
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-07-10 15:31:15 -0700

    Added StringExtensions

---
+++ tag_h/tag_h/Core/Helper/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace tag_h.Core.Helper.Extensions
+{
+    public class MatchFirstFailed : Exception { }
+
+    public static class StringExtensions
+    {
+        public static string MatchFirst(this string value, string regex)
+        {
+            try
+            {
+                return Regex.Match(value, regex).Groups[0].Captures[0].Value;
+            }
+            catch (Exception)
+            {
+                throw new MatchFirstFailed();
+            }
+        }
+    }
+}
---
+++ tag_h/tag_h_tests/Core/Helper/Extensions/StringExtensionsMatchTests.cs
@@ -0,0 +1,33 @@
+﻿using tag_h.Core.Helper.Extensions;
+
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace tag_h_tests.Core.Helper.Extensions
+{
+    public class StringExtensionsMatchTests
+    {
+        [Test]
+        public void MatchFirst_WithCorrectMatches_MatchesCorrectly()
+        {
+            AssertMatchSuccess("EasyExampleThing", @"(Example)", "Example");
+            AssertMatchSuccess("HttpMethodAttribute", @"Http(.*)Attribute", "Method");
+        }
+
+        [Test]
+        public void MatchFirst_WithIncorrectMatches_ThrowsMatchFirstFailed()
+        {
+            AssertMatchFailed("EasyBxampleThing", @"(Example)");
+        }
+
+        static private void AssertMatchSuccess(string input, string regex, string expected)
+        {
+            input.MatchFirst(regex).Should().Equals(expected);
+        }
+
+        static private void AssertMatchFailed(string input, string regex)
+        {
+            input.Invoking(value => value.MatchFirst(regex)).Should().Throw<MatchFirstFailed>();
+        }
+    }
+}
