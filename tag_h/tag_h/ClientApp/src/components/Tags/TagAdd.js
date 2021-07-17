b55987dc1f5c9a5962645128fa27e4a49010a72f
chally <chally-portable@DESKTOP-BUOJ68C>
2021-07-17 00:53:45 -0700

    Added tag add stib

---
+++ tag_h/tag_h/ClientApp/src/components/Tags/TagAdd.js
@@ -0,0 +1,23 @@
+﻿import React, { Component } from 'react';
+
+import { Controllers } from './../Framework/Controllers'
+
+export class Tag extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { tags: [] };
+    }
+
+    componentDidMount() {
+        this.state.tags = Controllers.Tags.GetAllTags();
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={() => this.callback()}> |+| </button>
+            </div>
+        );
+    }
+}
---
+++ tag_h/tag_h/Controllers/TagsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using tag_h.Core.Model;
+using tag_h.Core.Persistence;
+
+namespace tag_h.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TagsController : ControllerBase
+    {
+        private readonly ITagRepository _tagRepository;
+
+        public TagsController(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public TagSet GetAllTags()
+        {
+            return _tagRepository.GetAllTags();
+        }
+    }
+}
