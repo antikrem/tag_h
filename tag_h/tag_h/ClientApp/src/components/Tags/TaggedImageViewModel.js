f778fed50cd2bced8d1731315f8fb06858c7d194
chally <chally-portable@DESKTOP-BUOJ68C>
2021-07-22 05:58:49 -0700

    Added tagged image view model

---
+++ tag_h/tag_h/ClientApp/src/components/Tags/TaggedImageViewModel.js
@@ -0,0 +1,21 @@
+﻿import { Controllers } from './../../Framework/Controllers'
+
+export class TaggedImageViewModel {
+
+    constructor(image) {
+        this.image = image;
+    }
+
+    async getTags() {
+        return await Controllers.ImageTags.GetTags(this.image.uuid);
+    }
+
+    async deleteTag(tag) {
+        await Controllers.ImageTags.DeleteTag(this.image.uuid, tag.value);
+    }
+
+    async addTag(tag) {
+        await Controllers.ImageTags.AddTag(this.image.uuid, tag.value);
+    }
+
+}
