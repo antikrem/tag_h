52512ce475094dc645608f9c09d6352f986134dc
chally <chally-portable@DESKTOP-BUOJ68C>
2021-07-26 05:01:31 -0700

    Turned import view logic into a view model

--- tag_h/tag_h/ClientApp/src/components/Import/ImportView.js
+++ tag_h/tag_h/ClientApp/src/components/Import/ImportView.js
@@ -1,44 +1,22 @@
-﻿import React, { Component } from 'react';
+﻿import React from 'react';
 import { FileDrop } from 'react-file-drop';
 
-import { Controllers } from './../../Framework/Controllers'
+import "./ImportViewModel.js"
+const ImportView = (props) => {
+    const { files, addFiles } = ImportViewModel.use();
 
-export class ImportView extends Component {
-    static displayName = ImportView.name;
-
-    constructor(props) {
-        super(props);
-        this.state = { files: [] };
-    }
-
-    static toBase64 = file => new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result.replace("data:", "").replace(/^.+,/, ""));
-        reader.onerror = error => reject(error);
-    });
-
-    async addFiles() {
-        this.state.files.forEach(
-            async file => {
-                await Controllers.Images.AddImages([{ data: await ImportView.toBase64(file), fileName: file.name }])
-            }
-        );
-        this.setState({ files: [] })
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.files.map(file => <p>{file.name}</p >)}
-                <FileDrop
-                    onDrop={(files, _) => this.setState({ files: this.state.files.concat(Array.from(files)) })}
-                >
-                    Drop some files here!
+    return (
+        <div>
+            {files.map(file => <p>{file.name}</p >)}
+            <FileDrop
+                onDrop={ (files, _) => {files = files.concat(Array.from(files))} }
+            >
+                Drop some files here!
                 </FileDrop>
 
-                <button onClick={ () => this.addFiles() }>Submit</button>
-            </div>
-        );
-    }
+            <button onClick={ addFiles }>Submit</button>
+        </div>
+    );
 }
+
+export default ImportView;
--- tag_h/tag_h/ClientApp/src/components/Import/ImportViewModel.js
+++ tag_h/tag_h/ClientApp/src/components/Import/ImportViewModel.js
@@ -1,1 +1,24 @@
-﻿
+﻿import ViewModel from "react-use-controller";
+
+import { Controllers } from './../../Framework/Controllers'
+
+export default class TaggedImageViewModel extends ViewModel {
+
+    files = [];
+
+    static toBase64 = file => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result.replace("data:", "").replace(/^.+,/, ""));
+        reader.onerror = error => reject(error);
+    });
+
+    addFiles = async () => {
+        this.files.forEach(
+            async file => {
+                await Controllers.Images.AddImages([{ data: await ImportView.toBase64(file), fileName: file.name }])
+            }
+        );
+        this.files = []
+    }
+}
