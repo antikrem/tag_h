7c86fc38976bbb3f10aa029297bae966b8ca71ae
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-07-29 03:29:52 -0700

    Removed layout and sidebar components

--- tag_h/ClientApp/src/App.js
+++ tag_h/ClientApp/src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import ImagesView from './components/ImagesView';
 import { LogView } from './components/LogView';
 import Counter from './components/Counter';
-import { Image } from './components/Image';
 import ImportView from './components/Import/ImportView'
+import { Content } from './components/Content';
+import { SidebarItem } from './components/SidebarItem';
 
 import './custom.css'
+import './components/Sidebar.css';
+import './components/Layout.css';
 
 export default class App extends Component {
     static displayName = App.name;
 
     render() {
         return (
-            <Layout>
-                <Route exact path='/' component={Home} />
-                <Route path='/counter' component={Counter} />
-                <Route path='/images-view' component={ImagesView} />
-                <Route path='/log-view' component={LogView} />
-                <Route path='/image' component={Image} />
-                <Route path='/import-view' component={ImportView} />
-            </Layout>
+            <div className="layout">
+                <div className="sidebar">
+                    <button className="btn btn-primary" onClick={this.toggleOpen}>Close</button>
+                    <SidebarItem text="Home" link="/" />
+                    <SidebarItem text="Counter" link="/counter" />
+                    <SidebarItem text="Image View" link="/images-view" />
+                    <SidebarItem text="Import" link="/import-view" />
+                    <SidebarItem text="Logs" link="/log-view" />
+                </div>
+                <Content>
+                    <Home />
+                    <Counter />
+                    <ImagesView />
+                    <LogView />
+                    <ImportView />
+                </Content>
+            </div>
         );
     }
 }
