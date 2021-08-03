54fd50c16a358a836806540f9277c9143afa1112
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-08-03 02:19:53 -0700

    Added tile view to import

--- tag_h/ClientApp/src/components/Import/ImportView.js
+++ tag_h/ClientApp/src/components/Import/ImportView.js
@@ -1,22 +1,33 @@
 ﻿import React from 'react';
 import { FileDrop } from 'react-file-drop';
 
+import ImagesTiling from './../Images/ImagesTiling'
 import TagBox from './../Tags/TagBox';
 
 import ImportViewModel from './ImportViewModel';
 import ImportTagViewModel from './ImportTagViewModel';
+
+class TiledFileImage {
+
+    src = null;
+    tagViewmodel = null;
+
+    constructor(file) {
+        this.src = URL.createObjectURL(file);
+    }
+}
 
 const ImportView = (props) => {
     const { files, addFiles, setTags, submitFiles } = ImportViewModel.use();
 
     return (
         <div>
-            {files.map(file => <p>{file.name}</p >)}
             <FileDrop onDrop={ (newFiles, _) => addFiles(newFiles) } >
                 Drop some files here!
             </FileDrop>
             <button onClick={submitFiles}>Submit</button>
-            <TagBox viewmodel={ImportTagViewModel} setTags={setTags} />
+            {<TagBox viewmodel={ImportTagViewModel} setTags={setTags} />}
+            <ImagesTiling tiledImages={files.map(file => new TiledFileImage(file))} />
         </div>
     );
 }
