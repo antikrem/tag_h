04e23b37df0154fb48908a46273a9918b9b337a3
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-08-03 02:53:46 -0700

    Added task for adding images

--- tag_h/Controllers/ImagesController.cs
+++ tag_h/Controllers/ImagesController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,11 +6,10 @@ using Serilog;
 
 using tag_h.Core.Model;
 using tag_h.Core.Persistence;
+using tag_h.Core.Tasks;
 
 namespace tag_h.Controllers
 {
-    public record SubmittedFile(string Data, string FileName, List<Tag> Tags);
-
     [ApiController]
     [Route("[controller]")]
     public class ImagesController : ControllerBase
@@ -21,13 +19,21 @@ namespace tag_h.Controllers
         private readonly IHImageRepository _imageRepository;
         private readonly ITagRepository _tagRepository;
         private readonly IPhysicalImageProvider _physicalImageProvider;
+        private readonly ITaskRunner _taskRunner;
 
-        public ImagesController(ILogger logger, IHImageRepository imageRepository, ITagRepository tagRepository, IPhysicalImageProvider physicalImageProvider)
+        public ImagesController(
+                ILogger logger, 
+                IHImageRepository imageRepository, 
+                ITagRepository tagRepository, 
+                IPhysicalImageProvider physicalImageProvider,
+                ITaskRunner taskRunner
+            )
         {
             _logger = logger;
             _imageRepository = imageRepository;
             _tagRepository = tagRepository;
             _physicalImageProvider = physicalImageProvider;
+            _taskRunner = taskRunner;
         }
 
         [HttpGet]
@@ -53,12 +59,7 @@ namespace tag_h.Controllers
         [Route("[action]")]
         public void AddImages(List<SubmittedFile> files)
         {
-            files.ForEach(
-                    file => {
-                        var image = _imageRepository.CreateNewImage(file.FileName, Convert.FromBase64String(file.Data));
-                        file.Tags.ForEach(tag => _tagRepository.AddTagToImage(image, tag));
-                    }
-                );
+            _taskRunner.Submit(new AddNewImages(files));
         }
     }
 }
---
+++ tag_h/Core/Tasks/AddNewImages.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+using tag_h.Core.Model;
+using tag_h.Core.Persistence;
+
+
+namespace tag_h.Core.Tasks
+{
+    public record SubmittedFile(string Data, string FileName, List<Tag> Tags);
+
+    class AddNewImages : ITask
+    {
+        private readonly List<SubmittedFile> _files;
+
+        public string TaskName => "Adding New Images";
+
+        public AddNewImages(List<SubmittedFile> files)
+        {
+            _files = files;
+        }
+
+        public void Execute(IHImageRepository imageRepository, ITagRepository tagRepository)
+        {
+            _files.ForEach(
+                    file => {
+                        var image = imageRepository.CreateNewImage(file.FileName, Convert.FromBase64String(file.Data));
+                        file.Tags.ForEach(tag => tagRepository.AddTagToImage(image, tag));
+                    }
+                );
+        }
+    }
+}
