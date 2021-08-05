f931d48ec1f42466517e0226f79990d82e2777a0
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-08-06 02:36:10 +1000

    Added dummy helper

---
+++ tag_h_tests/_TestHelpers/Dummy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tag_h.Core.Helper.Extensions;
+
+namespace tag_h_tests._TestHelpers
+{
+    static class Dummy
+    {
+        private static Random _random = new Random();
+
+        public static int Int(int start = 0, int end = int.MaxValue) 
+            => _random.Next(start, end);
+
+        public static string String(int length = 32)
+            => Enumerable.Range(0, length).Select(_ => (char)_random.Next('a', 'z')).Concat();
+    }
+}
