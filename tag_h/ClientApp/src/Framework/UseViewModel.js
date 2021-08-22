d3813119584183b0e4b02696aca68f2b328dbfb1
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-08-22 16:58:15 +1000

    Added useViewModel

---
+++ tag_h/ClientApp/src/Framework/UseViewModel.js
@@ -0,0 +1,12 @@
+﻿import ViewModel from "react-use-controller";
+
+// Creates a ViewModel if entry extends ViewModel
+// Otherwise, returns the parameter
+export const useViewModel = function(entry, ...args) {
+    if (entry.use && entry.using) {
+        return entry.use(...args);
+    }
+    else {
+        return entry;
+    }
+}
