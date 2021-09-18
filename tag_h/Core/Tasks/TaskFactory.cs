479158f43400ec815c9f5d096dfeaf9b5c0353db
chally <chally-portable@DESKTOP-BUOJ68C>
2021-09-18 02:13:22 -0700

    Added task factory

---
+++ tag_h/Core/Tasks/TaskFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+using tag_h.Injection;
+
+
+namespace tag_h.Core.Tasks
+{
+    [Injectable]
+    public interface ITaskFactory
+    {
+        T CreateTask<T>() where T : ITask;
+    }
+
+    public class TaskFactory : ITaskFactory
+    {
+        private readonly IServiceLocator _serviceProvider;
+
+        public TaskFactory(IServiceLocator serviceLocator)
+        {
+            _serviceProvider = serviceLocator;
+        }
+
+        public T CreateTask<T>() where T : ITask
+        {
+            var constructor = typeof(T)
+                .GetConstructors()
+                .First();
+
+            var parameters = constructor
+                .GetParameters()
+                .Select(parameter => _serviceProvider.Resolve(parameter.ParameterType));
+
+            return (T)constructor.Invoke(parameters.ToArray());
+        }
+    }
+}
