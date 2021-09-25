c7deb5da0e42f1f6c94223f0f90cce5ac3831e45
chally <chally-portable@DESKTOP-BUOJ68C>
2021-09-24 23:09:11 -0700

    TEMP

---
+++ tag_h/ClientApp/src/components/Images/ImagesTilingTaskPane.js
@@ -0,0 +1,38 @@
+﻿import React from 'react'
+import ViewModel from "react-use-controller";
+
+export default class PanelViewModel extends ViewModel {
+
+    active = true;
+    panels = [];
+
+    constructor(panels) {
+        this.panels = panels;
+    }
+
+    getPanels = () => {
+        return this.panels;
+    }
+
+    setActive = (active) => {
+        this.active = active;
+    }
+}
+
+
+const ImagesTilingTaskPane = (props) => {
+
+    return (
+        <div className='image-task-pane scrollbar'>
+            <Tabs>
+                {props.panels.map(
+                    panel => {
+                        <Tab event={panel.name} title={panel.name}>
+                            {panel.element}
+                        </Tab>
+                    }
+                 )}
+             </Tabs>
+        </div>
+    );
+}
