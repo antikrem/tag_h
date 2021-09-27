5554c97f5c3800e172c31a220b481db0e03d3de0
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-09-27 22:28:38 +1000

    Added text search

--- tag_h/ClientApp/src/components/Tags/TagDropDown.js
+++ tag_h/ClientApp/src/components/Tags/TagDropDown.js
@@ -24,18 +24,39 @@ export class TagDropDown extends Component {
         super(props);
         this.callback = props.callback;
         this.selectedTags = props.selectedTags;
-        this.state = { tags: [] };
+        this.state = { tags: [], search: "" };
     }
 
     async componentDidMount() {
-        this.setState({ tags: await Controllers.Tags.GetAllTags() })
+        // TODO: use controller to do search
+        this.setState({ tags: await Controllers.Tags.GetAllTags(), search: this.state.search })
+
+        document.addEventListener('keydown', this.onKey);
+    }
+
+    async componentWillUnmount() {
+        document.removeEventListener('keydown', this.onKey);
+    }
+
+    onKey = (event) => {
+        const key = event.key.toLowerCase();
+        if ((/[a-zA-Z]/).test(key)) {
+            this.setState({ tags: this.state.tags, search: this.state.search + key });
+        }
     }
 
     render() {
+
+        var tags = this.state.tags;
+        tags = tags.filter(tag => !this.selectedTags.map(tag => tag.id).includes(tag.id));
+        if (this.state.search != "") { // TODO: make this iterate over once
+            tags = tags.filter(tag => tag.name.indexOf(this.state.search) >= 0);
+        }
+
         return (
             <div>
-                {this.state.tags.filter(tag => !this.selectedTags.map(tag => tag.id).includes(tag.id)).map(
-                    tag => <TagDropDownElement tag={tag} callback={ this.callback } />
+                {tags.map(
+                    tag => <TagDropDownElement key={tag.id} tag={tag} callback={ this.callback } />
                 )}
             </div>
         );
