8b6b95928a01a806e5a0a2b6db504f97c4a2a513
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-10-05 00:47:45 +1000

    Added reflective type generator

---
+++ tag_h/Injection/Typing/ReflectiveGenerationSpec.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+
+using tag_h.Core.Helper.Extensions;
+
+using TypeGen.Core.Extensions;
+using TypeGen.Core.SpecGeneration;
+
+
+namespace tag_h.Injection.Typing
+{
+    public class ReflectiveGenerationSpec : GenerationSpec
+    {
+        public override void OnBeforeGeneration(OnBeforeGenerationArgs args)
+        {
+            //TODO: use ReflectionHelper
+            GetType()
+                .Assembly
+                .GetLoadableTypes()
+                .Where(type => type.IsDefined(typeof(UsedByClient), false))
+                .ForEach(Register);
+        }
+
+        private void Register(Type type)
+        {
+            AddClass(type);
+        }
+    }
+}
--- tag_h/Startup.cs
+++ tag_h/Startup.cs
@@ -24,7 +24,6 @@ namespace tag_h
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddHttpContextAccessor();
 
