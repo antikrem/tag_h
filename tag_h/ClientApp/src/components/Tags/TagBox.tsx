263127c0668f6e62570b02fc6e1302514e497cec
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-12-27 20:58:35 +1000

    Added tag adding logic

--- tag_h/ClientApp/src/components/Tags/TagBox.tsx
+++ tag_h/ClientApp/src/components/Tags/TagBox.tsx
@@ -6,22 +6,32 @@ export interface TagBoxProps<T> {
     add: (tag: T) => void;
     remove: (tag: T) => void;
     render: (tag: T) => ReactNode;
+    comparator: (first: T, second: T) => boolean;
 }
 
-export const TagBox = <T, >(props: TagBoxProps<T>) => {
+export const TagBox = <T,>(props: TagBoxProps<T>) => {
     const [selected, setSelected] = useState([] as T[]);
     const [all, setAll] = useState([] as T[]);
+
+    const [dropped, setDropped] = useState(false);
 
     useEffect(() => setSelected(props.selected), [props.selected]);
     useEffect(() => setAll(props.all), [props.all]);
 
     return (
-        <div style={{ display: 'inline' }}>
-            {selected.map((tag, index) =>
-                <Tag key={index} remove={() => props.remove(tag)}>
-                    {props.render(tag)}
-                </Tag>
-            )}
+        <div>
+            <div style={{ display: 'inline' }}>
+                {selected.map((tag, index) =>
+                    <Tag key={index} remove={() => props.remove(tag)}>
+                        {props.render(tag)}
+                    </Tag>
+                )}
+                <button onClick={() => setDropped(!dropped)}> |{dropped ? "x" : "+"}| </button>
+            </div>
+            {dropped && <TagDropDown
+                options={all.filter(tag => !selected.find(t => props.comparator(t, tag)))}
+                add={props.add}
+                render={props.render} />}
         </div>
     );
 }
@@ -31,11 +41,29 @@ interface TagProps<T> {
     children: ReactNode;
 }
 
-const Tag = <T, >(props: TagProps<T>) => {
+const Tag = <T,>(props: TagProps<T>) => {
     return (
-            <span style={{ marginRight: '5px', padding: '5px', whiteSpace: 'nowrap',  backgroundColor: 'red' }}>
-                <p style={{ padding: '3px', marginBottom: '5px', display: 'inline-block' }}> {props.children}</p>
-                <p style={{ padding: '3px', marginBottom: '5px', display: 'inline-block', cursor: 'pointer' }} onClick={props.remove}> x </p>
-            </span>
-        );
+        <span style={{ marginRight: '5px', padding: '5px', whiteSpace: 'nowrap', backgroundColor: 'red' }}>
+            <p style={{ padding: '3px', marginBottom: '5px', display: 'inline-block' }}> {props.children}</p>
+            <p style={{ padding: '3px', marginBottom: '5px', display: 'inline-block', cursor: 'pointer' }} onClick={props.remove}> x </p>
+        </span>
+    );
+}
+
+interface TagDropDown<T> {
+    options: T[],
+    add: (tag: T) => void;
+    render: (tag: T) => ReactNode;
+}
+
+const TagDropDown = <T,>(props: TagDropDown<T>) => {
+    return (
+        <div>
+            {props.options.map(
+                (tag, index) => <div key={index} onClick={() => props.add(tag)}>
+                    {props.render(tag)}
+                </div>)
+            }
+        </div>
+    );
 }
