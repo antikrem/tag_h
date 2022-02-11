a89c004676a6fdcddd79290403df9624ee2c28e0
Chalinda Rodrigo <nobleservant49@gmail.com>
2022-02-12 03:11:03 +1000

    Converted tag repository

---
+++ tag_h/Core/Repositories/TagRepository.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+using EphemeralEx.Extensions;
+using EphemeralEx.Injection;
+
+using tag_h.Core.Model;
+using tag_h.Persistence;
+using tag_h.Persistence.Model;
+
+
+namespace tag_h.Core.Repositories
+{
+    [Injectable]
+    public interface ITagRepository
+    {
+        Tag CreateTag(string name, IEnumerable<string> values);
+
+        TagSet GetAllTags();
+
+        Tag? SearchTag(string value);
+    }
+
+    public class TagRespository : ITagRepository
+    {
+        private readonly IDatabase _database;
+        private readonly ITagFactory _tagFactory;
+
+        public TagRespository(IDatabase database, ITagFactory tagFactory)
+        {
+            _database = database;
+            _tagFactory = tagFactory;
+        }
+
+        public Tag CreateTag(string name, IEnumerable<string> values)
+        {
+            var state = _database.CreateTag(name);
+            values.ForEach(value => _database.AddValue(state, value));
+            return _tagFactory.Create(state);
+        }
+
+        public TagSet GetAllTags()
+        {
+            return _tagFactory.Create(_database.GetAllTags());
+        }
+
+        public Tag? SearchTag(string value)
+        {
+            var state = _database.SearchTag(value);
+            return state != null 
+                ? _tagFactory.Create(state) 
+                : null;
+        }
+    }
+}
